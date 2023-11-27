cb416c5e93d2c70b88708ffc8e44659314a0f6c8
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-11-27 21:54:12 +0700

    fate: temp commit

--- src/queue.pl
+++ src/queue.pl
@@ -1,17 +1,35 @@
-:-dynamic(queueName/1).
+:- dynamic(queueName/1).
+:- dynamic(traversal/1).
+:- dynamic(listName/1).
 
-enqueueName:-
+enqueueName :-
     read(Name),
     assertz(queueName(Name)).
 
-dequeueName:-
+dequeueName :-
     retract(queueName(Name)),
     write(Name),
     assertz(queueName(Name)), !.
 
-addEnd([], X, [X]):-!.
-addEnd([A|B], X, [A|D]):- addEnd(B,C,D).
+addEnd([], X, [X]) :- !.
+addEnd([A|B], X, [A|D]) :- addEnd(B, X, D).
 
-clearQueue:-
-    retractall(queueName(List)),
-    write([List]),!.
+clearQueue :-
+    retractall(listName(_)),
+    ListNm = [],
+    assertz(listName(ListNm)),
+    retractall(traversal(_)),
+    queueName(_),
+    repeat,
+    retract(queueName(Name)),
+    retract(listName(ListName)),
+    addEnd(ListName, Name, AddedList),
+    assertz(listName(AddedList)),
+    \+queueName(_),
+    !, listName(ListNeme), write(ListNeme).
+    
+
+readNumber:-
+    repeat, write('Masukkan banyak pemain: '),
+    read(Angka),
+    Angka >= 2, Angka =< 4, !.
