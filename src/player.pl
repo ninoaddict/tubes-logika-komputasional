ea31a9f8cb519f5bda940678859853d874f1bd3f
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-11-28 21:25:52 +0700

    fix: turn file cleanup

--- src/player.pl
+++ src/player.pl
@@ -27,45 +27,8 @@ checkWin(Player) :-
             retractall(isInit(_)), retractall(isPlayTheGame(_)),
             format('Player ~w telah menguasai dunia', [Player]), !.
 
-/* Still in Progress */
-readPlayerNumber:-
-    write('Masukkan jumlah pemain: '),
-    read(X),
-    (X >= 2, X =< 4 ->
-        retractall(nbPlayer(_)),
-        assertz(nbPlayer(X)),
-        nl
-    ;   write('Mohon masukkan angka antara 2 - 4.'),
-        nl,
-        checkPlayer
-    ).
-
-/* Still in Progress */
-readPlayerNames:-
-    _N is 4,
-    _i is 1,
-    repeat,
-    write('Masukkan nama pemain '),
-    write(_i),
-    write(': '),
-    nl,
-    _i is _i + 1,
-    endOfReadPlayerName(_i, _N).
-    
-endOfReadPlayerName(N, N).
-
 /* Access the front name of the queue*/
 currentPlayer(X) :- player(X), !.
-
-/* Move the front name to the back of the queue*/
-dequeuePlayer(X) :- 
-    retract(queueName(Name)),
-    assertz(queueName(Name)),
-    !.
-
-/* Clear the player queue (used in the end of the game) */
-clearPlayerQueue(X) :- 
-    retractall(queueName(_)),!.
 
 /* check Player Detail */
 checkPlayerDetail(X):-
