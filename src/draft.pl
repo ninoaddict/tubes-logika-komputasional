f64d80b33c82e2d4248f0c2343460b06bb32624c
Berto Richardo Togatorop <bertotogatorop27@gmail.com>
2023-11-29 19:29:13 +0700

    feat: add more verbose to draft

--- src/draft.pl
+++ src/draft.pl
@@ -1,8 +1,8 @@
 /* b. draft */
 draft(Territory, TroopsCount) :- isPlayTheGame(_), territoryContinent(_Continent, Territory), TroopsCount > 0, !,
-                 (currentPlayer(CurrPlayer),  ownedTerritory(Territory, CurrPlayer , CurrentTerritoryTroops) -> 
-                    (unplacedSoldier(CurrPlayer, UnplacedSoldier),
-                    (UnplacedSoldier >= TroopsCount) -> 
+                 currentPlayer(CurrPlayer),  
+                 (ownedTerritory(Territory, CurrPlayer , CurrentTerritoryTroops), unplacedSoldier(CurrPlayer, UnplacedSoldier) ->
+                    ((UnplacedSoldier >= TroopsCount) -> 
                         (NewNbTroops is TroopsCount + CurrentTerritoryTroops,
                         NewUnplacedTroops is UnplacedSoldier - TroopsCount, setUnplacedSoldier(CurrPlayer, NewUnplacedTroops),
                         setOwnedTerritory(Territory, CurrPlayer, NewNbTroops ), nl,
@@ -10,5 +10,6 @@ draft(Territory, TroopsCount) :- isPlayTheGame(_), territoryContinent(_Continent
                         format('Tentara total di ~w: ~w', [Territory, NewNbTroops]), nl,
                         format('Jumlah Pasukan Tambahan Player ~w: ~w', [CurrPlayer, NewUnplacedTroops]),nl
                         ) 
-                    ; (nl, write('Jumlah tentara tambahan Anda kurang.'))) 
-                ; (nl, write('Territory ini bukan milik Anda.'))).
+                    ; (nl, write('Pasukan tidak mencukupi.'), nl,
+                        format('Jumlah pasukan tambahan player ~w: ~w', [CurrPlayer, UnplacedSoldier]))) 
+                ; (nl, format('Player ~w tidak memiliki wilayah ~w', [CurrPlayer, Territory]))).
