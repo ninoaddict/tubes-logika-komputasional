8d17757509c3f59ffeff4a818760eaed7046469c
Berto Richardo Togatorop <bertotogatorop27@gmail.com>
2023-11-29 16:43:35 +0700

    fix: fix troopsCount

--- src/teritory.pl
+++ src/teritory.pl
@@ -1,18 +1,20 @@
+/* Write semua tetangga yang sudah di-pass dari list */
 writeNeighbours([]) :- fail.
 writeNeighbours([H|[]]) :-
     territoryName(H, SlangName), format('~w ', [SlangName]).
 writeNeighbours([H|T]) :- 
     territoryName(H, SlangName), format('~w, ', [SlangName]), writeNeighbours(T).
 
-checkLocationDetail(Teritory) :- 
+/* Check detail dari suatu lokasi */
+checkLocationDetail(Territory) :- 
             isPlayTheGame(_),
-            ownedTerritory(Teritory, Owner, NbTroops), 
-            format('Kode            : ~w',[Teritory]), nl,
-            territoryName(Teritory, Name),
+            ownedTerritory(Territory, Owner, NbTroops), 
+            format('Kode            : ~w',[Territory]), nl,
+            territoryName(Territory, Name),
             format('Nama            : ~w',[Name] ),nl,
             format('Pemilik         : ~w',[Owner]), nl,
             format('Total Tentara   : ~w',[NbTroops]), nl,
-            adjacentList(Teritory, NeighbourList), 
+            adjacentList(Territory, NeighbourList), 
             write('Tetangga        : '), writeNeighbours(NeighbourList), !.
 
 
